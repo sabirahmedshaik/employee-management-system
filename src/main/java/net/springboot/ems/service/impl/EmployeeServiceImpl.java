79c08357fe090ad41e2640421ac21ece80885151
sabirahmedshaik <sabirahmed.piya@gmail.com>
2024-05-04 19:46:33 +0530

    Implement Add Employee

--- src/main/java/net/springboot/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/springboot/ems/service/impl/EmployeeServiceImpl.java
@@ -1,12 +1,22 @@
 package net.springboot.ems.service.impl;
 
+import lombok.AllArgsConstructor;
 import net.springboot.ems.dto.EmployeeDto;
+import net.springboot.ems.entity.Employee;
+import net.springboot.ems.mapper.EmployeeMapper;
+import net.springboot.ems.repository.EmployeeRepository;
 import net.springboot.ems.service.EmployeeService;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
+    private EmployeeRepository employeeRepository;
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
-        return null;
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savesEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 }
