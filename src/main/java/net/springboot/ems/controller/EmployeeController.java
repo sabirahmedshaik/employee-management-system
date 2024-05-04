fa8a3d144fe0e6538cd72c373b9f3f6518dcd0e2
sabirahmedshaik <sabirahmed.piya@gmail.com>
2024-05-04 20:10:04 +0530

    Create EmployeeController and Build Add Employee REST API

---
+++ src/main/java/net/springboot/ems/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package net.springboot.ems.controller;
+
+import lombok.AllArgsConstructor;
+import net.springboot.ems.dto.EmployeeDto;
+import net.springboot.ems.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}
